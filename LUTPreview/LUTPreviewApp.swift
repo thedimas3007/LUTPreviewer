28af68cd25d32db6ba6d16a13fe290431d953dea
thedimas3007 <dima.orischenko@gmail.com>
2022-12-23 18:17:30 +0100

    Initial Commit

---
+++ LUTPreview/LUTPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LUTPreviewApp.swift
+//  LUTPreview
+//
+//  Created by Dima on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LUTPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
